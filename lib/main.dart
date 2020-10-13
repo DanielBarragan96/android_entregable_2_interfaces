aa758baaa043c599ed2da064e8f4386821b03a96
DanielBarragan96 <ie702554@iteso.mx>
2020-10-13 13:02:56 -0500

    changes implemented for log in

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
+import 'package:entregable_2/colors.dart';
+import 'package:entregable_2/login/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      title: "Practica integradora 2",
+      theme: ThemeData(
+        primaryColor: kMainPurple,
       ),
+      home: LoginPage(),
     );
   }
 }
