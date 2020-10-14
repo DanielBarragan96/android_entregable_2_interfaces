c7d5328edcf9528c1c1ab2b10cd58b308d8da9d4
DanielBarragan96 <ie702554@iteso.mx>
2020-10-14 00:12:13 -0500

    added project colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -6,5 +6,5 @@ const Color kLightestPurple = const Color(0xFF8377D4);
 const Color kBlack = const Color(0xFF2A2A2A);
 const Color kLightBlack = const Color(0xFF3D3D3D);
 const Color kDarkGray = const Color(0xFF5F5F5F);
-const Color kLightGray = const Color(0xFFCCCCCC);
+const Color kLightGray = const Color(0x8FCCCCCC);
 const Color kWhite = const Color(0xFFFFFFFF);
